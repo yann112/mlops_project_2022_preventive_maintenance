b364e9818750d3bcb6b2da3a5e6f5b8aa3c57dc7
EC2 Default User <ec2-user@ip-172-31-32-224.eu-west-3.compute.internal>
2022-12-24 12:06:51 +0000

    aws setup

---
+++ aws_setup.sh
@@ -0,0 +1,12 @@
+sudo yum -y install docker
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+chmod +x kubectl
+mkdir -p ~/.local/bin
+mv ./kubectl ~/.local/bin/kubectl
+kubectl version --client
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+git clone https://github.com/yann112/mlops_project_2022_preventive_maintenance
